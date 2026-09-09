d75a69e88aabd5c5c7cbc2c12f1e3eb0e957255e
DovzhukTimofii <121349911+DovzhukTimofii@users.noreply.github.com>
2026-09-09 15:16:41 +0300

    Initialize persisted authentication on profile page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,13 +1,21 @@
-﻿using MauiStartup.ViewModels;
+using MauiStartup.ViewModels;
 
 namespace MauiStartup;
 
 public partial class MainPage : ContentPage
 {
+    private readonly MainPageModel _viewModel;
+
     public MainPage(MainPageModel viewModel)
     {
         InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
 
-        BindingContext = viewModel;
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.InitializeAsync();
     }
 }
