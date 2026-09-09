f67ef6e546110ac91d7b76e404eab24c69921a54
DovzhukTimofii <121349911+DovzhukTimofii@users.noreply.github.com>
2026-09-09 15:14:44 +0300

    Add authenticated user model

---
+++ Models/AuthUser.cs
@@ -0,0 +1,9 @@
+namespace MauiStartup.Models;
+
+public class AuthUser
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PictureUrl { get; set; } = string.Empty;
+}
